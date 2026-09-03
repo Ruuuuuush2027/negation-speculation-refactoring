f7c43c08a6c062211f3c3c343050c55aa9db297b
Mo Jiang <mojiang@usc.edu>
2026-09-03 15:19:54 -0700

    revised examples and pipeline, happy yay

--- pipeline_example.py
+++ pipeline_example.py
@@ -1,15 +1,15 @@
 from pipeline import get_cue_and_scope, format_result
 
 def main():
-    examples = [
-        # Negation. example from training.
-        "They analyzed 146 prokaryotic genomes , but no likely tRNA of the "
-        "novel amino acid was detected .",
-        # Speculation ("should", "may") and negation ("not") in one sentence.
-        "It may happen to be noted that the degree distribution is not maintained .",
-        # No cue at all
-        "The samples were incubated for thirty minutes .",
-    ]
+    # examples = [
+    #     # Negation. example from training.
+    #     "They analyzed 146 prokaryotic genomes , but no likely tRNA of the "
+    #     "novel amino acid was detected .",
+    #     # Speculation ("should", "may") and negation ("not") in one sentence.
+    #     "It may happen to be noted that the degree distribution is not maintained .",
+    #     # No cue at all
+    #     "The samples were incubated for thirty minutes .",
+    # ]
 
     text = "I'm not sure I handled that conversation well, and I probably should've apologized sooner."
     print(get_cue_and_scope(text))
